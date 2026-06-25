2a1f19e3d6fce3244464fc3c81a3d29d032a4e88
ravithelavi <ravithelavi@gmail.com>
2026-06-25 11:34:11 +0530

    Remove legacy deployment scripts

--- scripts/login.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-ROOT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
-source "$ROOT_DIR/scripts/config.sh"
-
-ssh -t -p "$SSH_PORT" "$SSH_USER@$SSH_HOST" \
-"cd '$REMOTE_PROJECT' && exec bash -l"
