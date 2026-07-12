df8a9da038ef0100a1bf4ea426fe483a5e2c228f
ravithelavi <ravithelavi@gmail.com>
2026-07-12 17:53:56 +0530

    refactor(customer360): restore compact overflow menu layout

--- app/Support/Customer360/Customer360OverflowMenuLucideIcon.php
+++ app/Support/Customer360/Customer360OverflowMenuLucideIcon.php
@@ -41,7 +41,7 @@ final class Customer360OverflowMenuLucideIcon
     $paths = self::paths()[$name] ?? self::paths()['circle'];
 
     return sprintf(
-      '<svg class="%s" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" aria-hidden="true">%s</svg>',
+      '<svg class="%s" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.75" stroke-linecap="round" stroke-linejoin="round" aria-hidden="true">%s</svg>',
       e($class),
       $paths,
     );
--- resources/views/customer-360/partials/overflow-menu-item.blade.php
+++ resources/views/customer-360/partials/overflow-menu-item.blade.php
@@ -22,8 +22,10 @@
             data-workspace-context="customer"
             @if(filled($item['workspaceActionType'] ?? null)) data-workspace-action-type="{{ $item['workspaceActionType'] }}" @endif
             @if(filled($item['shortcut'] ?? null)) data-c360-shortcut-action="{{ $item['shortcut'] }}" @endif>
-        {!! Customer360OverflowMenuLucideIcon::render($item['icon']) !!}
-        <span class="c360-quick-toolbar-more-item-label">{{ $item['label'] }}</span>
+        <span class="c360-quick-toolbar-more-item-main">
+            {!! Customer360OverflowMenuLucideIcon::render($item['icon']) !!}
+            <span>{{ $item['label'] }}</span>
+        </span>
     </button>
 @elseif($itemType === 'communication')
     <button type="button"
@@ -33,8 +35,10 @@
             data-workspace-communication-action-key="{{ $item['communicationActionKey'] }}"
             data-workspace-incident-id="{{ $incident->id }}"
             data-workspace-context="customer">
-        {!! Customer360OverflowMenuLucideIcon::render($item['icon']) !!}
-        <span class="c360-quick-toolbar-more-item-label">{{ $item['label'] }}</span>
+        <span class="c360-quick-toolbar-more-item-main">
+            {!! Customer360OverflowMenuLucideIcon::render($item['icon']) !!}
+            <span>{{ $item['label'] }}</span>
+        </span>
     </button>
 @elseif($itemType === 'tab')
     <button type="button"
@@ -42,22 +46,28 @@
             role="menuitem"
             data-c360-overflow-tab="{{ $item['tab'] }}"
             @if(filled($item['anchor'] ?? null)) data-c360-overflow-anchor="{{ $item['anchor'] }}" @endif>
-        {!! Customer360OverflowMenuLucideIcon::render($item['icon']) !!}
-        <span class="c360-quick-toolbar-more-item-label">{{ $item['label'] }}</span>
+        <span class="c360-quick-toolbar-more-item-main">
+            {!! Customer360OverflowMenuLucideIcon::render($item['icon']) !!}
+            <span>{{ $item['label'] }}</span>
+        </span>
     </button>
 @elseif($itemType === 'status')
     <span @class([...$itemClasses, 'c360-quick-toolbar-more-item--status'])
           role="menuitem"
           aria-disabled="true">
-        {!! Customer360OverflowMenuLucideIcon::render($item['icon']) !!}
-        <span class="c360-quick-toolbar-more-item-label">{{ $item['label'] }}</span>
+        <span class="c360-quick-toolbar-more-item-main">
+            {!! Customer360OverflowMenuLucideIcon::render($item['icon']) !!}
+            <span>{{ $item['label'] }}</span>
+        </span>
     </span>
 @else
     <a href="{{ $item['href'] }}"
        @class($itemClasses)
        role="menuitem"
        @if(str_starts_with((string) ($item['href'] ?? ''), 'http')) target="_blank" rel="noopener noreferrer" @endif>
-        {!! Customer360OverflowMenuLucideIcon::render($item['icon']) !!}
-        <span class="c360-quick-toolbar-more-item-label">{{ $item['label'] }}</span>
+        <span class="c360-quick-toolbar-more-item-main">
+            {!! Customer360OverflowMenuLucideIcon::render($item['icon']) !!}
+            <span>{{ $item['label'] }}</span>
+        </span>
     </a>
 @endif
